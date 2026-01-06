b95065339253f6897c05de74edef321b4d01c376
jorgeorozco21 <jo296019@gmail.com>
2026-01-05 19:31:36 -0600

    Seccion de Descripcion de Competicion Empezada

---
+++ resources/views/components/competicion.blade.php
@@ -0,0 +1,11 @@
+<section class="w-full h-[100px] bg-[#3B9D36] flex flex-col items-center justify-center">
+    <div class="w-[90%] flex flex-col items-center justify-center">
+        <h2 class="text-2xl text-white font-bold"> Premier League </h2>
+        <hr class="w-full border-1 border-white mt-2">
+    </div>
+    <div class="w-[90%] flex items-center justify-center">
+        <p class="text-white mt-2"> Temporada 2023/2024 </p>   
+        <img src="" alt="Logo de Competicion">
+    </div>
+</section>
+
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/competicion', function () {
+    return view('competicion');
+});
